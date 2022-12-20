286703ac22727f69505be26a252ab42605254144
Clint McMahon <clint@cmcmahon.io>
2022-12-20 16:04:59 -0600

    updates

---
+++ MediaGuide.Web/Pages/Disclaimer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MediaGuide.Web.Pages;
+
+public class DisclaimerModel : PageModel
+{
+    private readonly ILogger<DisclaimerModel> _logger;
+
+    public DisclaimerModel(ILogger<DisclaimerModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
